b362d0aace62297b6e81b995f44d24fb47dda186
KouernDoch <dochkouern@gmail.com>
2024-09-14 15:05:06 +0700

    Initial Commit

---
+++ ServerCommunication/ServerCommunicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServerCommunicationApp.swift
+//  ServerCommunication
+//
+//  Created by Kourn Doch on 14/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ServerCommunicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
