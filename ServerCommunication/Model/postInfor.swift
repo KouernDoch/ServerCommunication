6cdda6ea2768299267ffa84e1ec05e8fe1ae3fb9
KouernDoch <dochkouern@gmail.com>
2024-09-16 18:25:33 +0700

    first

---
+++ ServerCommunication/Model/postInfor.swift
@@ -0,0 +1,8 @@
+//
+//  postInfor.swift
+//  ServerCommunication
+//
+//  Created by Kourn Doch on 16/9/24.
+//
+
+import Foundation
---
+++ ServerCommunication/PostView.swift
@@ -0,0 +1,18 @@
+//
+//  PostView.swift
+//  ServerCommunication
+//
+//  Created by Kourn Doch on 16/9/24.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostView()
+}
